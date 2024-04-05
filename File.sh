2958bf7d1a72d69459309a08ebe164b5cce2c511
Luis Giordano <14226200+h4cker39@users.noreply.github.com>
2024-04-05 14:56:36 -0300

    Update File.sh

--- File.sh
+++ File.sh
@@ -1,25 +1,34 @@
 #/bin/sh
-
+ 
 #Better way of finding XSS vulnerabilites
 #Using Gau (Wayback machine endpoints obsolete, crawling and live host)
 #Script by Luis C F Giordano
 #FR0$T
 #
 #
-
-
-subfinder -d $1 | tee subfinder.txt
-
+while getopts d: flag
+do
+    case "${flag}" in
+        d) domain=${OPTARG};;
+    esac
+done
+echo "target is: $domain";
+subfinder -d $domain | tee subfinder.txt
 httpx -l subfinder.txt -o httpx.txt
-
-echo $1 | gau --threads 5 >> Endpoints.txt
-
+echo $domain | gau --threads 5 >> Endpoints.txt
 cat httpx.txt | katana -jc >> Endpoints.txt
-
-cat Enpoints.txt | uro >> Endpoints_F.txt
-
+cat Endpoints.txt | uro >> Endpoints_F.txt
 cat Endpoints_F.txt | gf xss >> XSS.txt
-
 cat XSS.txt | Gxss -p khXSS -o XSS_Ref.txt
-
-dalfox file XSS_Ref.txt -o Vulnerable_XSS.txt
+torsocks dalfox file XSS_Ref.txt -o Vulnerable_XSS.txt
+ 
+#clean up
+ 
+#
+rm Endpoints.txt
+rm subfinder.txt
+rm Vulnerable_XSS.txt
+rm XSS.txt 
+ 
+rm Endpoints_F.txt
+rm Endpoints.txt
